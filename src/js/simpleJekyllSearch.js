35522f0d7fd158298e2dd1d59b35b00a9e6102a3
Victor Gazotti <vtr.gomes@hotmail.com>
2019-08-03 14:17:33 -0300

    Alterando retorno de busca

--- src/js/simpleJekyllSearch.js
+++ src/js/simpleJekyllSearch.js
@@ -7,7 +7,7 @@
             searchResults   : '.search-results',
             searchResultsTitle   : '<h4>Resultados da busca:</h4>',
             limit           : '10',
-            noResults       : '<p>OOPS!<br/><small>Não encontramos nada 😢</small></p>'
+            noResults       : '<p>OOPS!<br/><small>Não encontramos nada</small></p>'
         }, options);
 
         var properties = settings.jsonFormat.split(',');
