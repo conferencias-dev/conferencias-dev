0dfb4f4eca9aac3ff9ade66868c5563e0a6a8324
Alex Assunção <alexjjassuncao@gmail.com>
2019-08-03 15:29:59 -0300

    Update simpleJekyllSearch.js

--- src/js/simpleJekyllSearch.js
+++ src/js/simpleJekyllSearch.js
@@ -7,7 +7,7 @@
             searchResults   : '.search-results',
             searchResultsTitle   : '<h4>Resultados da busca:</h4>',
             limit           : '10',
-            noResults       : '<p>OOPS!<br/><small>Não encontramos nada</small></p>'
+            noResults       : '<p>OOPS!<br/><small>Não encontramos nada.</small></p>'
         }, options);
 
         var properties = settings.jsonFormat.split(',');
